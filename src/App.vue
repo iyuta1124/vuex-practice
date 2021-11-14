8e03a7c51dd421866204248e2b97d8a6fa6d85d5
iyuta1124 <iyuta1124@gmail.com>
2021-11-14 14:57:14 +0900

    vuexの勉強

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <Calender />
+</template>
+
+<script>
+import Calender from "./components/Calender.vue";
+
+export default {
+  name: "App",
+  components: {
+    Calender,
+  },
+  
+};
+</script>
---
+++ src/components/Calender.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <p>Hello</p>
+    {{ doubleCount }}
+    <button @click="increment">+2</button>
+    <input type="text" v-model="message" />
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+import moment from "moment";
+import { mapGetters } from "vuex";
+export default {
+  mounted() {
+    let date = moment().startOf("month");
+    const dayOfWeek = date.day();
+    console.log(dayOfWeek);
+  },
+  computed: {
+    ...mapGetters(["doubleCount"]),
+    message: {
+      get() {
+        return this.$store.getters.message;
+      },
+      set(value) {
+        this.$store.dispatch("updateMessage", value);
+      },
+    },
+  },
+  methods: {
+    increment() {
+      this.$store.dispatch("increment", 2);
+    },
+  },
+};
+</script>
